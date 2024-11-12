2359624e40ed5a6a2d00510411fb7c62cc3438e0
Alejandro Lagos <flagos.rd@gmail.com>
2024-11-12 13:50:45 -0600

    Added theme and entry point

---
+++ app/src/main/java/com/flagos/news/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.flagos.news
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                /*val navController = rememberNavController()
+                MainNavHost(
+                    navController = navController,
+                    modifier = Modifier.padding(8.dp)
+                )*/
+            }
+        }
+    }
+}
